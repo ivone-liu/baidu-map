f714753ebad2396b77db4000b22e4f69a0fa06e1
ivone <i@ivone.me>
2022-04-27 15:34:11 +0800

    * init

---
+++ src/Eagle/TrackUpload.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Desc:
+ * Author: Ivone <i@ivone.me>
+ * Date: 2022/4/27
+ * Time: 15:06
+ */
+
+namespace BaiduMap\Eagle;
+
+
+use GuzzleHttp\Client;
+
+class TrackUpload
+{
+
+    protected $ak;
+    protected $service_id;
+    protected $entity;
+
+    private $req_url = "http://yingyan.baidu.com/api/v3/track/addpoints";
+
+    public function __construct($ak, $service_id, $entity) {
+        $this->ak = $ak;
+        $this->service_id = $service_id;
+        $this->entity = $entity;
+    }
+
+    /**
+     * Desc: 加点
+     * Author: Ivone <i@ivone.me>
+     * Date: 2022/4/27
+     * Time: 15:14
+     * @param array $point, 经纬度 [longitude, latitude]
+     */
+    public function addpoint(Array $point, $time, $coord = 'wgs84') {
+        $package = [
+            'ak'                =>  $this->ak,
+            'service_id'        =>  $this->service_id,
+            'entity_name'       =>  $this->entity,
+            'latitude'          =>  $point[0],
+            'longitude'         =>  $point[1],
+            'loc_time'          =>  $time,
+            'coord_type_input'  =>  $coord
+        ];
+        return $this->send($package);
+    }
+
+    /**
+     * Desc: 批量增加
+     * Author: Ivone <i@ivone.me>
+     * Date: 2022/4/27
+     * Time: 15:23
+     * @param array $data
+     */
+    public function addpoint_batch(Array $data) {
+
+    }
+
+    /**
+     * Desc: 暂不需要额外封装
+     * Author: Ivone <i@ivone.me>
+     * Date: 2022/4/27
+     * Time: 15:18
+     * @param $
+     */
+    protected function buildPackage(&$package) {}
+
+
+    protected function send(Array $package) {
+        $http_client = new Client();
+        $baidu_resp = $http_client->request('POST', $this->req_url, [
+            'form_params' =>    $package
+        ]);
+        return $baidu_resp;
+    }
+
+}
