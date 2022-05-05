bf8e32403ceb6a9ceccc7e4a658f1eed5d5a26dd
ivone <i@ivone.me>
2022-05-05 14:05:40 +0800

    * 测试读取围栏

--- src/Tools/Request.php
+++ src/Tools/Request.php
@@ -12,11 +12,15 @@ function curl_post($url, Array $package) {
 }
 
 function curl_get($url, Array $package) {
+    $params = "";
+    foreach ($package as $key=>$item) {
+        $params .= $key . "=" . $item . "&";
+    }
+    $params = substr($params,0,strlen($params)-1);
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt_array($curl, $package);
+    curl_setopt($curl, CURLOPT_URL, $url . "?" . $params);
     $res = curl_exec($curl);
     curl_close($curl);
     return $res;
