ff61dbe08982df47421fc107518dfa912139c6eb
ivone <i@ivone.me>
2022-05-25 14:35:46 +0800

    + 轨迹纠偏

--- src/Path/Rectify.php
+++ src/Path/Rectify.php
@@ -13,14 +13,14 @@ class Rectify
 
     protected $ak;
 
-    private $track_url = "http://api.map.baidu.com/rectify/v1/track?";
+    private $track_url = "http://api.map.baidu.com/rectify/v1/track";
 
     public function __construct($ak) {
         $this->ak = $ak;
     }
 
     public function Rectify(Array $ponits) {
-        $baidu_resp = curl_get($this->track_url, [
+        $baidu_resp = curl_post($this->track_url, [
             'ak'                =>  $this->ak,
             'rectify_option'    =>  "need_mapmatch:1|transport_mode:driving|denoise_grade:1|vacuate_grade:1",
             'monitored_person'  =>  json_encode($ponits)
