9b94d8dacd199bced9a515076ee94b6a1410c8c3
ivone <i@ivone.me>
2022-05-25 14:24:07 +0800

    + 轨迹纠偏

---
+++ src/Path/Rectify.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Desc:
+ * Author: Ivone <i@ivone.me>
+ * Date: 2022/5/25
+ * Time: 14:20
+ */
+
+namespace BaiduMap\Path;
+
+class Rectify
+{
+
+    protected $ak;
+
+    private $track_url = "http://api.map.baidu.com/rectify/v1/track?";
+
+    public function __construct($ak) {
+        $this->ak = $ak;
+    }
+
+    public function Rectify(Array $ponits) {
+        $baidu_resp = curl_get($this->track_url, [
+            'ak'                =>  $this->ak,
+            'rectify_option'    =>  "need_mapmatch:1|transport_mode:driving|denoise_grade:1|vacuate_grade:1",
+            'monitored_person'  =>  json_encode($ponits)
+        ]);
+        return callbackResponse($baidu_resp);
+    }
+
+}
