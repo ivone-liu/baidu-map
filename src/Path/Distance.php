e3f33dc8419800bbd5dde7ad995a6f188822a199
ivone <i@ivone.me>
2022-06-16 14:39:22 +0800

    + distance

---
+++ src/Path/Distance.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Desc:
+ * Author: Ivone <i@ivone.me>
+ * Date: 2022/6/16
+ * Time: 14:29
+ */
+
+namespace BaiduMap\Path;
+
+class Distance
+{
+
+    protected $ak;
+    protected $service;
+    protected $entity;
+
+    private $track_url = "http://yingyan.baidu.com/api/v3/track/getdistance";
+
+    public function __construct($ak, $service, $entity) {
+        $this->ak = $ak;
+        $this->service = $service;
+        $this->entity = $entity;
+    }
+
+    public function Distance($start_time, $end_time) {
+        $baidu_resp = curl_post($this->track_url, [
+            'ak'                =>  $this->ak,
+            'service_id'        =>  $start_time,
+            'entity_name'       =>  $this->entity,
+            'start_time'        =>  $start_time,
+            'end_time'          =>  $end_time,
+            'is_processed'      =>  1
+        ]);
+        return callbackResponse($baidu_resp);
+    }
+
+
+}
